846c302518b97bf78120b6cc3a1db1b0e3e4fa79
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-03-15 19:47:26 +0200

    Tehtävä 1.1-1.2 valmis

--- osa_1/tehtavat/tehtava_1.1/src/App.jsx
+++ osa_1/tehtavat/tehtava_1.1/src/App.jsx
@@ -1,3 +1,41 @@
+// 3 komponenttia: Header, Content ja Footer.
+
+/** HEADER *****************/
+const Header = ({ course }) => <h1>{course}</h1>;
+/*************************** */
+
+/* CONTENT ****************/
+// Part-komponentti:
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+);
+
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+};
+/****************************** */
+
+/* TOTAL **********************/
+const Total = ({ totalExercises }) => {
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+/***************************** */
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +47,18 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+
+      <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
